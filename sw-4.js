6cf0642507bf7b19c3cda472975c866fdf91abf5
robs2002019 <robs2002019@gmail.com>
2026-06-29 19:07:50 -0600

    Add files via upload

---
+++ sw-4.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'robs-delivery-v1';
+const ASSETS = [
+  '/robsdelivery/',
+  '/robsdelivery/index.html',
+  '/robsdelivery/manifest.json',
+  '/robsdelivery/icon-192.png',
+  '/robsdelivery/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
